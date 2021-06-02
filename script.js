c55b4e856b5502467672520d242f54fb41ed6695
Erez <erezfree29@gmail.com>
2021-06-01 20:06:43 -0700

    add calculations

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
 
-// function makeGrid(rows,cols) {
-//   const container = document.querySelector('.container');
-//   let row = document.createElement('div');
-//   row.className = 'row';
-//   for (i = 0; i < rows; i++) {
-//     let row = document.createElement('div');
-//     row.className = 'row';
-//     for (f = 0; f < cols; f++) {
-//       let cell = document.createElement('div');
-//       cell.className = 'cell';
-//       cell.innerHTML = f+1
-//       row.appendChild(cell);
-//     }
-//     container.appendChild(row);
-//   }
-//   container.addEventListener("mouseover", mouseOver);
-//   container.addEventListener("mouseout", mouseOut);
-// }
-
-// makeGrid(16, 16);
-// function mouseOver() {
-//   const container = document.querySelector('.container');
-//   container.style.color = "blue";
-// }
-
-// function mouseOut() {
-//   const container = document.querySelector('.container');
-//   container.style.color = "black";
-// }
-
-var equation = ['1'];
-
-
-
-function dis(equation,val) {
-  localStorage.setItem('number', val);
-  display = document.querySelector('.screen');
-  display.innerHTML = val
-  equation.push(val)
-  localStorage.setItem('input', JSON.stringify(equation));
-  console.log(JSON.parse(localStorage.getItem('input')));
+var equation = [];
+display = document.querySelector('.screen')
+function dis(val) {
+  if (display.innerHTML == '0') {
+    display.innerHTML = val;
+   } else if (val != '+' && val != '-'   && val != '*'   && val != '/' 
+    && display.innerHTML != '+'  && display.innerHTML != '-'  && display.innerHTML != '*'  && display.innerHTML != '/') {
+    display.innerHTML += val
+  } else if  (val == '+' || val == '-'   || val == '*'   || val == '/'){
+    equation.push(display.innerHTML)
+    equation.push(val)  
+    display.innerHTML = val
+  } else if (display.innerHTML == '+' || display.innerHTML == '+'  || display.innerHTML == '*'  || display.innerHTML == '/' ){
+    display.innerHTML = val;
+  }
+  
 }
 
+function solve () {
+  equation.push(display.innerHTML)
+  console.log(equation)
+  result = parseFloat(equation[0])
+  for (i = 0; i < equation.length; i+= 2) {
+    if (equation[i+1] == '+') {
+      let number =  parseFloat(equation[i+2]);
+      result += parseFloat(equation[i+2]);
+    } else if (equation[i+1] == '-') {
+      result -= parseFloat(equation[i+2]);
+    } else if (equation[i+1] == '*') {
+      result = result *  parseFloat(equation[i+2]);
+    } else if (equation[i+1] == '/') {
+      result = result /  parseFloat(equation[i+2]);
+    }
+
+  }
+  display.innerHTML = result;
+}
 
